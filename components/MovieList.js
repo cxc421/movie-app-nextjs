1c1ef5afe6de4a3e15d1b3896cf08642d46eaf52
Chris Chuang <chrischuang0421@gmail.com>
2020-12-17 17:50:03 +0800

    Add fake data to MovieList

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,166 +1,34 @@
-const MovieList = () => (
+function shorten(text, maxLength = 50) {
+  if (text && text.length > maxLength) {
+    return text.substr(0, maxLength) + "...";
+  }
+  return text;
+}
+
+const MovieList = ({ movies = [] }) => (
   <>
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="#">
-          <img
-            className="card-img-top"
-            src="http://placehold.it/700x400"
-            alt=""
-          />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">Item One</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            numquam aspernatur!
-          </p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">
-            &#9733; &#9733; &#9733; &#9733; &#9734;
-          </small>
+    {movies.map(({ id, name, image, description, rating }) => (
+      <div className="col-lg-4 col-md-6 mb-4" key={id}>
+        <div className="card h-100">
+          <a href="#">
+            <img className="card-img-top" src={image} alt="" />
+          </a>
+          <div className="card-body">
+            <h4 className="card-title">
+              <a href="#">{name}</a>
+            </h4>
+            {/* <h5>$24.99</h5> */}
+            <p className="card-text">{shorten(description, 100)}</p>
+          </div>
+          <div className="card-footer">
+            {/* <small className="text-muted">
+              &#9733; &#9733; &#9733; &#9733; &#9734;
+            </small> */}
+            <small className="text-muted">{rating}</small>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="#">
-          <img
-            className="card-img-top"
-            src="http://placehold.it/700x400"
-            alt=""
-          />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">Item Two</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            numquam aspernatur! Lorem ipsum dolor sit amet.
-          </p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">
-            &#9733; &#9733; &#9733; &#9733; &#9734;
-          </small>
-        </div>
-      </div>
-    </div>
-
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="#">
-          <img
-            className="card-img-top"
-            src="http://placehold.it/700x400"
-            alt=""
-          />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">Item Three</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            numquam aspernatur!
-          </p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">
-            &#9733; &#9733; &#9733; &#9733; &#9734;
-          </small>
-        </div>
-      </div>
-    </div>
-
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="#">
-          <img
-            className="card-img-top"
-            src="http://placehold.it/700x400"
-            alt=""
-          />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">Item Four</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            numquam aspernatur!
-          </p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">
-            &#9733; &#9733; &#9733; &#9733; &#9734;
-          </small>
-        </div>
-      </div>
-    </div>
-
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="#">
-          <img
-            className="card-img-top"
-            src="http://placehold.it/700x400"
-            alt=""
-          />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">Item Five</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            numquam aspernatur! Lorem ipsum dolor sit amet.
-          </p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">
-            &#9733; &#9733; &#9733; &#9733; &#9734;
-          </small>
-        </div>
-      </div>
-    </div>
-
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="#">
-          <img
-            className="card-img-top"
-            src="http://placehold.it/700x400"
-            alt=""
-          />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">Item Six</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            numquam aspernatur!
-          </p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">
-            &#9733; &#9733; &#9733; &#9733; &#9734;
-          </small>
-        </div>
-      </div>
-    </div>
+    ))}
   </>
 );
 
--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,6 +1,6 @@
-const SideMenu = () => (
+const SideMenu = ({ appName = "" }) => (
   <>
-    <h1 className="my-4">Shop Name</h1>
+    <h1 className="my-4">{appName}</h1>
     <div className="list-group">
       <a href="#" className="list-group-item">
         Category 1
