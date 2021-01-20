127b12ed660821ecb0ecaa2c3543fcda472576ff
Chris Chuang <chrischuang0421@gmail.com>
2021-01-20 17:45:37 +0800

    Fix incorrect import file name

--- pages/movies/[id]/edit.js
+++ pages/movies/[id]/edit.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { MovieCreateForm } from "../../../components/movieCreateForm";
+import { MovieCreateForm } from "../../../components/MovieCreateForm";
 import { getMovieById, updateMovie } from "../../../actions";
 
 const EditMovie = ({ movie }) => {
